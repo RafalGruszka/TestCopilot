d560c19c81d150012f40852ec76fa10dedff24e6
RafalGruszka <rafal.gruszka@outlook.com>
2023-11-05 17:24:57 +0100

    To jest pierwsza wersja testu GitHub Copilot - generacja funkcji dodającej dwa argumenty

---
+++ main.py
@@ -0,0 +1,25 @@
+# Create function adding two numeric arguments
+def add(num1, num2):
+    return num1 + num2
+
+# Ask user to input num1, num2
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Print the result
+print("The sum of {0} and {1} is {2}".format(num1, num2, add(num1, num2)))
+
+# End of program
+
+
+
+
+
+
+
+
+
+
+
+
+
